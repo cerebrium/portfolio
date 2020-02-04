43aa184576a0709e0540f19b4f0b3f6f91358143
cerebrium <nicholas.m.shankland@gmail.com>
2020-02-03 16:32:21 -0800

    got the shadows!

--- src/components/twitterProject.js
+++ src/components/twitterProject.js
@@ -15,7 +15,7 @@ const TwitterProject = () => {
       }
     `)
     return (<div className='imageStyler'>
-      <a href="https://todoleest.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Img fluid={data.fileName.childImageSharp.fluid} alt="../images/twitterApp.png"/></a>
+      <a href="https://todoleest.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Img fluid={data.fileName.childImageSharp.fluid} alt="../images/twitterApp.png" className='imagePicture'/></a>
     </div>
     )
 }
